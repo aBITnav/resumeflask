633c5f6bde47e7e63de80cb84c84d6765131f513
User <User@macs-MacBook-Pro.local>
2022-01-13 22:04:53 +0530

    no Prod works with localhost

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from parser import parse
+
+app=Flask(__name__)
+
+@app.route('/',methods = ['POST', 'GET'])
+def hello():
+    if request.method=='GET':
+        return '<h5> Server Started </h5>'
+    else:
+        resume=request.files['resume']
+        json=parse(resume)
+        return jsonify(json)
+
+if __name__ == '__main__':
+    app.run()
